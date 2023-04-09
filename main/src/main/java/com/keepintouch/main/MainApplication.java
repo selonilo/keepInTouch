fa2dfaae32ed6fab39324658ba53ec11eea8e620
Salih Çetin <salihcetinteam@gmail.com>
2023-04-09 12:14:40 +0300

    Add openapi config

--- main/src/main/java/com/keepintouch/main/MainApplication.java
+++ main/src/main/java/com/keepintouch/main/MainApplication.java
@@ -1,14 +1,19 @@
 package com.keepintouch.main;
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class MainApplication {
 
 	public static void main(String[] args) {
@@ -16,12 +21,26 @@ public class MainApplication {
 	}
 
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
+	public OpenAPI customOpenAPI(@Value("TODO") String description,
+								 @Value("1.0") String version){
+		return new OpenAPI()
+				.info(new Info()
+						.title("TODO")
+						.version(version)
+						.description(description)
+						.license(new License().name("TODO")));
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+
+			}
+		};
 	}
 
 }
